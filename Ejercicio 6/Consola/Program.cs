e1f6e8aa71cdbd51449c9f4c654b69998d8fb573
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-10 19:54:35 -0300

    Commit Ejercicio 6

---
+++ Ejercicio 6/Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 6: Determinar si un número es positivo.
+Console.Write("Ingrese un número: ");
+int numero;
+
+if (!Int32.TryParse(Console.ReadLine(), out numero))
+{
+    Console.Write("Error, debe ingresar un número correcto");
+}
+else
+{
+    if (numero > 0)
+    {
+        Console.Write("El número es positivo");
+    }
+    else
+    {
+        Console.Write("El número es negativo");
+    }
+}
