c2125ebf7b28627659481d5bbbe0fbdd7cb71b51
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-10 19:33:48 -0300

    Commit Ejercicio 3

---
+++ Ejercicio 3/Consola/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 3: Calcular el área de un cuadrado.
+Console.Write("Ingrese la medida del lado: ");
+double lado1 = double.Parse(Console.ReadLine());
+
+double area = lado1 * lado1;
+Console.Write("El área es de: " + area + " cm");
