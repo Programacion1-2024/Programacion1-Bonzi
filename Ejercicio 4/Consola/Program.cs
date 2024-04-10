90b33e61222eb813324941847031a7f9741f5b07
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-10 19:38:33 -0300

    Commit Ejercicio 4

---
+++ Ejercicio 4/Consola/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 4: Calcular el promedio de tres números.
+Console.Write("Ingrese el primer número a promediar: ");
+double n1 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número a promediar: ");
+double n2 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el tercer número a promediar: ");
+double n3 = double.Parse(Console.ReadLine());
+
+double promedio = (n1 + n2 + n3) / 3;
+Console.Write("El promedio de los tres número es: " + promedio);
