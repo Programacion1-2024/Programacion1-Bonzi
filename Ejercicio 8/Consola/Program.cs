528bc92f3150de776477d86c7444e84e3322b3cb
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-10 20:09:58 -0300

    Ejercicio 8

---
+++ Ejercicio 8/Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 8: Calcular el perímetro de un triángulo.
+Console.Write("Ingrese la medida del lado 1: ");
+double lado1 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese la medida del lado 2: ");
+double lado2 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese la medida del lado 3: ");
+double lado3 = double.Parse(Console.ReadLine());
+
+double perimetro = lado1 + lado2 + lado3;
+Console.Write("El perímetro del triángulo es: " + perimetro);
+
