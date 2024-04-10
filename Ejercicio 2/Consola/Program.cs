5d9223a30b6868bd863666a9e88296bde9c594f5
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-10 19:31:29 -0300

    Commit Ejercicio 2

---
+++ Ejercicio 2/Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ejercicio 2: Determinar si un número es par o impar.
+
+Console.Write("Ingrese un número: ");
+int numero = Int32.Parse(Console.ReadLine());
+if (numero % 2 == 0)
+{
+    Console.Write("El número es par");
+}
+else
+{
+    Console.Write("El número es impar");
+}
+
