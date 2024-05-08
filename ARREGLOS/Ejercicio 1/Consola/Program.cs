f404ef385f308c9a728438828e253c40a7f611de
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-05-08 20:51:13 -0300

    Commit ejercicios ARREGLOS

---
+++ ARREGLOS/Ejercicio 1/Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 1: Ingresar datos en un arreglo y mostrarlos. Consigna: El usuario debe ingresar 5 números enteros. El programa debe almacenar estos números en un arreglo y luego mostrarlos en la consola.
+
+int[] numeros = new int[5];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write("Ingrese un número: ");
+    if (!int.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("Error. Ingrese un valor válido.");
+    }
+}
+for (int i = 0;i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento {i}: {numeros[i]}");
+}
+
---
+++ ARREGLOS/Ejercicio 2/Consola/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Ejercicio 2: Calcular promedio de notas utilizando un arreglo. Consigna: El usuario debe ingresar 5 notas. El programa debe calcular el promedio de estas notas y mostrarlo en la consola. Utiliza un arreglo para almacenar las notas.
+
+double suma = 0;
+double promedio = 0;
+double[] notas = new double[5];
+
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.Write("Ingrese la nota: ");
+    if (double.TryParse(Console.ReadLine(), out double nota))
+    {
+        notas[i] = nota;
+    }
+}
+for (int i = 0;i < notas.Length; i++)
+{
+    suma += notas[i];   
+    promedio = suma / 5;
+}
+Console.Write($"El promedio de las notas es de: {promedio}");
