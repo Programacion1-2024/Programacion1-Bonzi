7a86f414fb036da04e2997b181b892495a499f29
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-05-11 20:30:30 -0300

    Commit ejercicios LISTAS

---
+++ LISTAS/Ejercicio 3/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingresar 5 números enteros.");
+
+List<int> numeros = new List<int>();
+int numero;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Ingresar número {i+1}: ");
+    if (!int.TryParse( Console.ReadLine(), out numero))
+    {
+        Console.WriteLine("Error. Ingrese un valor válido. Intente de nuevo.");
+        i--;
+    }
+    else
+    {
+        numeros.Add(numero);
+    }
+}
+foreach (int i in numeros)
+{
+    Console.WriteLine($"Número: {i}");
+}
---
+++ LISTAS/Ejercicio 4/Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingresar 5 notas.");
+
+List<float> notas = new List<float>();
+float nota;
+float suma = 0;
+float promedio = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Ingresar nota {i+1}: ");
+    if (!float.TryParse(Console.ReadLine()?.Replace(",", "."), out nota))
+    {
+        Console.WriteLine("Error. Ingrese un valor válido. Intente de nuevo.");
+        i--;
+    }
+    else
+    {
+        notas.Add(nota);
+        suma += nota;
+        promedio = suma / 5;
+    }
+}
+Console.Write($"El promedio de las notas es de: {promedio}");
