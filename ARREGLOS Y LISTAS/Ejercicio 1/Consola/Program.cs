25169ac48cec334c40a33caa840caef0d4ea2566
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-05-11 21:06:21 -0300

    Commit ejercicios ARREGLOS Y LISTAS

---
+++ ARREGLOS Y LISTAS/Ejercicio 1/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 1 - Ordenar palabras alfabéticamente. El usuario ingresará una lista de palabras. El programa debe guardar, ordenar estas palabras alfabéticamente y mostrarlas en la consola.
+
+List <string> nombres = new List<string>(); 
+
+while (true)
+{
+    Console.Write("Ingresar la palabra: ");
+    string? palabra = Console.ReadLine();
+    if (palabra != " ")
+    {
+        nombres.Add(palabra);
+    }
+    else
+    {
+        break;
+    }
+}
+nombres.Sort();
+Console.WriteLine("Palabras ordenadas alfabeticamente: ");
+foreach (string nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
---
+++ ARREGLOS Y LISTAS/Ejercicio 2/Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 2- Filtrar números pares en una lista. El usuario ingresará números enteros uno por uno. El programa debe solicitar al usuario que ingrese los números hasta que ingrese el número 0 para finalizar. Luego, debe mostrar los números ingresados de mayor a menor en la consola. Si se ingresa un valor no válido, se mostrará un mensaje de error.
+
+Console.WriteLine("Ingresar números enteros. Finalizar con 0.");
+
+List<int> numeros = new List<int>();
+
+int numero = 1;
+
+while(numero != 0)
+{
+    Console.Write("Ingresar un número: ");
+    if (!int.TryParse(Console.ReadLine(), out numero))
+    {
+        Console.WriteLine("Error. Ingrese un valor válido.");
+        numero = 1;
+    }
+    else if (numero != 0)
+    {
+        numeros.Add(numero);
+    }
+}
+var numerosOrdenados = numeros.OrderBy(num => num).ToList();
+
+Console.WriteLine("Números ordenados de mayor a menor:");
+foreach (int i in numerosOrdenados)
+{
+    Console.WriteLine(i);
+}
+
