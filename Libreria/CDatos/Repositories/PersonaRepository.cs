35953337225ed1303f8304faff5e9a3e2e37b4d2
Trinidad <trinidad.bonzi.ices@gmail.com>
2024-08-12 15:12:18 -0300

    Librería

---
+++ Libreria/CDatos/Repositories/PersonaRepository.cs
@@ -0,0 +1,16 @@
+﻿using CDatos.Contexts;
+using CDatos.Repositories.Contracts;
+using CEntidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDatos.Repositories
+{
+    public class PersonaRepository : Repository<Persona>, IPersonaRepository
+    {
+        public PersonaRepository(LibreriaContext context) : base(context) { }
+    }
+}
---
+++ Libreria/CLogica/Contracts/IPersonaLogic.cs
@@ -0,0 +1,17 @@
+﻿using CEntidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLogica.Contracts
+{
+    public interface IPersonaLogic
+    {
+        void AltaPersona(Persona persona);
+        void ActualizacionPersona(string documento, Persona personaActualizar);
+
+        void EliminarPersona(string documento);
+    }
+}
